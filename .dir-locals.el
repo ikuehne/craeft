f2c56cb2e7474bb487b6c20c984d494928bcd89e
ikuehne <ikuehne617@gmail.com>
2017-06-14 13:18:12 -0700

    Remove emacs crap.

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c++-mode
-  (flycheck-clang-args `("-lllvm" ,(shell-command-to-string "llvm-config --cxxflags")))
-  (canary . "tweet, tweet!")
-  (flycheck-clang-language-standard . "c++14")))
-
-(setq flycheck-clang-args `(,@(split-string (shell-command-to-string "llvm-config --cxxflags")) "--std=c++14"))
